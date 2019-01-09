87197e22b3e7c94aa44286c28d8e4ddd007e5514
njohnson <njohnson@k12.com>
2019-01-09 18:21:18 -0500

    routing fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,6 @@ const routes: Routes = [
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
   },
-  { path: 'schedule', loadChildren: './schedule/schedule.module#SchedulePageModule' },
   { path: 'announcements', loadChildren: './page/announcements/announcements.module#AnnouncementsPageModule' },
   { path: 'schedule', loadChildren: './page/schedule/schedule.module#SchedulePageModule' },
   { path: 'classes', loadChildren: './page/classes/classes.module#ClassesPageModule' }
