ab49d03deb903e552fedb345aeec89524cd6ae66
njohnson <njohnson@k12.com>
2019-01-09 18:16:29 -0500

    adding images and some page stubs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'schedule', loadChildren: './schedule/schedule.module#SchedulePageModule' },
+  { path: 'announcements', loadChildren: './page/announcements/announcements.module#AnnouncementsPageModule' },
+  { path: 'schedule', loadChildren: './page/schedule/schedule.module#SchedulePageModule' },
+  { path: 'classes', loadChildren: './page/classes/classes.module#ClassesPageModule' }
 ];
 
 @NgModule({
