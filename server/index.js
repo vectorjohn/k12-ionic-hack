660d41e1a78b9b1e1cee153dd09ebe034158ce8e
John Reeves <jreeves@k12.com>
2019-01-10 15:20:49 -0800

    logging in chat server

--- server/index.js
+++ server/index.js
@@ -4,22 +4,27 @@ let io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
 
-    socket.on('disconnect', function(){
-    io.emit('users-changed', {user: socket.nickname, event: 'left'});
-});
+    console.log('NEW CONNECTION');
 
-socket.on('set-nickname', (nickname) => {
-    socket.nickname = nickname;
-io.emit('users-changed', {user: nickname, event: 'joined'});
-});
+    socket.on('disconnect', function () {
+        console.log('Disconnect: ', socket.nickname);
+        io.emit('users-changed', {user: socket.nickname, event: 'left'});
+    });
 
-socket.on('add-message', (message) => {
-    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});
-});
+    socket.on('set-nickname', (nickname) => {
+        console.log('set-nickname', nickname);
+        socket.nickname = nickname;
+        io.emit('users-changed', {user: nickname, event: 'joined'});
+    });
+
+    socket.on('add-message', (message) => {
+        console.log('msg:', socket.nickname, message.text);
+        io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});
+    });
 });
 
 var port = process.env.PORT || 3001;
 
-http.listen(port, function(){
+http.listen(port, function () {
     console.log('listening in http://localhost:' + port);
 });
