5c289a4e63de67963b4af0a83d54f22173f0b73d
joshtippett <tippett@gmail.com>
2019-01-10 13:49:01 -0500

    Fixed BaseService loading

--- src/app/services/classes/classes.service.ts
+++ src/app/services/classes/classes.service.ts
@@ -4,14 +4,17 @@ import {StudentClass, StudentClassResponse} from '../../models/student-class';
 import {Observable} from 'rxjs/index';
 import {BaseService} from '../base.service';
 import {NetworkService} from '../network/network.service';
+import {Storage} from '@ionic/storage';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ClassesService extends BaseService<StudentClass[], StudentClassResponse> {
 
-    constructor(protected http: HttpClient, protected networkService: NetworkService) {
-        super(http, networkService, '/assets/mocks/classes.json');
+    constructor(protected http: HttpClient,
+                protected networkService: NetworkService,
+                protected storage: Storage) {
+        super(http, networkService, storage, '/assets/mocks/classes.json');
     }
 
     getClasses(): Observable<StudentClass[]> {
--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -3,14 +3,17 @@ import {BaseService} from '../base.service';
 import {NotificationItem} from '../../models/notification-item';
 import {HttpClient} from '@angular/common/http';
 import {NetworkService} from '../network/network.service';
+import {Storage} from '@ionic/storage';
 
 @Injectable({
     providedIn: 'root'
 })
 export class NotificationService extends BaseService<NotificationItem, NotificationItem> {
 
-    constructor(protected http: HttpClient, protected networkService: NetworkService) {
-        super(http, networkService, '/assets/mocks/notifications.json');
+    constructor(protected http: HttpClient,
+                protected networkService: NetworkService,
+                protected storage: Storage) {
+        super(http, networkService, storage, '/assets/mocks/notifications.json');
     }
 
     protected transform(response: NotificationItem): NotificationItem {
