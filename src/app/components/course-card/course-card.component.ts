99ea361e64a6d050d57bd6812a9207f0b3fb12cb
njohnson <njohnson@k12.com>
2019-01-10 21:54:34 -0500

    chat at me, bro

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ToastController} from '@ionic/angular';
 
 @Component({
     selector: 'app-course-card',
@@ -13,12 +14,25 @@ export class CourseCardComponent implements OnInit {
     @Input() grade: string;
     @Input() percentage: number;
     @Input() image: string;
+
     // Assignments?
 
 
-    constructor() {
+    constructor(public toastController: ToastController) {
     }
 
     ngOnInit() {
+    }
+
+    startDownload(n: string): void {
+        this.presentToast(n);
+    }
+
+    async presentToast(n: string) {
+        const toast = await this.toastController.create({
+                message: 'Downloading ' + n + 'for offline use',
+                duration: 2000
+            });
+        toast.present();
     }
 }
