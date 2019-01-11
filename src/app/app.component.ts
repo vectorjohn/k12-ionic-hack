75ce6ecb1c03411abf2213756443e7009b163618
John Reeves <jreeves@k12.com>
2019-01-10 17:02:44 -0800

    a toast that reloads when dismissed now appears with new updates, I hope.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 
-import {Platform} from '@ionic/angular';
+import {Platform, ToastController} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
     selector: 'app-root',
@@ -34,7 +35,9 @@ export class AppComponent {
 
     constructor(private platform: Platform,
                 private splashScreen: SplashScreen,
-                private statusBar: StatusBar) {
+                private statusBar: StatusBar,
+                private updates: SwUpdate,
+                private toast: ToastController) {
         this.initializeApp();
     }
 
@@ -42,6 +45,19 @@ export class AppComponent {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
+            this.updates.available.subscribe(() => {
+                this.toast.create({
+                    color: 'success',
+                    message: 'A new version is available',
+                    showCloseButton: true,
+                    closeButtonText: 'Reload'
+                }).then(toast => {
+                    toast.present();
+                    return toast.onDidDismiss();
+                }).then(() => {
+                    document.location.reload();
+                });
+            });
         });
     }
 }
